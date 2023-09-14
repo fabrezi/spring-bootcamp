13a68de106184edffdba92605f40a67b91c6dcdd
Mowgli <fkschon@gmail.com>
2023-09-14 01:40:40 -0400

    Create sum_three_digits.java

---
+++ coding_prep/sum_three_digits.java
@@ -0,0 +1,20 @@
+public class sum_three_digits {
+
+    /*
+    Meta interview questions.
+     */
+
+    static int sum(int A, int B){
+        int z = A + B;
+        return z;
+    }
+
+    public static void main(String[] args){
+        int A = 1;
+        int B = 2;
+        System.out.println("the sume is:" + sum(A,B));
+    }
+
+
+
+}
