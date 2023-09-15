d70e451e416d4b538266a3b5acf6c85e92c609d5
Mowgli <fkschon@gmail.com>
2023-09-15 15:10:25 -0400

    Update wrong_answer.java

--- coding_prep/wrong_answer.java
+++ coding_prep/wrong_answer.java
@@ -1,20 +1,25 @@
-class Solution {
-  
-  public String getWrongAnswers(int N, String C) {
-    // Write your code here
-    StringBuilder sb = new StringBuilder(C);
-    //String S = sb.toString();
-    for(int i=0; i < N; i++){
-     if(C.charAt(i) == 'A'){
-       sb.replace(i,N,"B");
-     }else if(C.charAt(i) == 'B'){
-       sb.replace(i,N,"A");
-     }
-    }
-    String S = sb.toString();
-    return S;
-  }
-  
-}
+public class wrong_answer {
 
+    public static String wrong(int N, String C){
+        StringBuilder str = new StringBuilder(C);
+
+        for(int i=0; i < N; i++){
+            if(C.charAt(i)=='A'){
+                str.replace(i,N,"B");
+            } else if(C.charAt(i)=='B'){
+                str.replace(i,N,"A");
+            }
+        }
+        String str1 = str.toString();
+        return str1;
+    }
+
+    public static void main(String[] args){
+        int N = 3;
+        String C = "AAA";
+
+        System.out.println(wrong(N,C));
+    }
+
+}
 
