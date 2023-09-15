f7342d24792ac6978c3ba0ac98f10c0f2be95931
Mowgli <fkschon@gmail.com>
2023-09-15 12:28:31 -0400

    Update wrong_answer.java

--- coding_prep/wrong_answer.java
+++ coding_prep/wrong_answer.java
@@ -1,39 +1,20 @@
-public class wrong_answer {
-
-    //Meta Interview Prep
-
-    //Input: ABABA
-    //ALGO: Convert the String into opposite characters
-    //Output: BABAB
-
-
-    public static void main(String [] args){
-        String C = "BBB";
-        int N = 3;
-        //String newname = C.substring(0,1) + 'x' + C.substring(2);
-        //System.out.println(newname);
-
-        StringBuilder sb = new StringBuilder(C);
-        //sb.append("Hoola");
-
-        //System.out.println(sb);
-        //System.out.println(N);
-
-        for(int i =0; i < N; i++){
-            if (C.charAt(i) == 'A') {
-                //sb.append('B');
-                sb.replace(i,N,"B");
-            } else if(C.charAt(i)  == 'B'){
-                sb.replace(i,N,"A");
-            }
-            //System.out.println(sb);
-        }
-
-        System.out.println(sb);
-
-
-        }
-
+class Solution {
+  
+  public String getWrongAnswers(int N, String C) {
+    // Write your code here
+    StringBuilder sb = new StringBuilder(C);
+    //String S = sb.toString();
+    for(int i=0; i < N; i++){
+     if(C.charAt(i) == 'A'){
+       sb.replace(i,N,"B");
+     }else if(C.charAt(i) == 'B'){
+       sb.replace(i,N,"A");
+     }
     }
+    String S = sb.toString();
+    return S;
+  }
+  
+}
 
 
