b5f2b4fba2cbcb058b13851da86ef398caadafa3
fabrezi <fkschon@gmail.com>
2023-12-18 14:15:25 -0500

    alltech train01

--- coding_prep/hailstone.java
+++ coding_prep/hailstone.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class hailstone {
 
-    public static int Seq(int initial_value){
-        //int [] arr = new int[100];
+    public static List<Integer> Seq(int n, int initial_value, int [] arr){
+        //int [] arr = new int [n];
+        //int counter = 0;
+        List<Integer> list = new ArrayList<Integer>();
 
         while(initial_value != 1) {
             if (initial_value % 2 == 0) {
                 initial_value = initial_value / 2;
+                 //counter = counter + 1;
+                //arr.insert(initial_value) ;
             } else {
                 initial_value = (3 * initial_value) + 1;
             }
@@ -16,8 +23,11 @@ public class hailstone {
 
 
     public static void main(String[] args){
-        int initial_value = 10;
-        System.out.println(Seq(initial_value));
+        int n = 10;
+        int initial_value = 11;
+        int [] arr = new int [2];
+        //List<Integer> list = new ArrayList<Integer>();//initialization of an arraylist.
+        System.out.println(Seq(n, initial_value, arr));
     }
 
 }
---
+++ coding_prep/insert_arr.java
@@ -0,0 +1,12 @@
+public class insert_arr {
+
+    public static void main(String [] args){
+        int [] arr = new int [10];
+        arr[1] = 10;
+        arr[2] = 2;
+
+        System.out.println(arr);
+    }
+
+
+}
