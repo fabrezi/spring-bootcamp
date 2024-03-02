f19de63ea53ff893d180d3609268a6f57a72caed
fabrezi <fkschon@gmail.com>
2024-03-01 22:38:27 -0500

    clean up

--- coding_prep/avg_temp_finder.java
+++
@@ -1,7 +0,0 @@
-public class avg_temp_finder {
-
-    //read from the randnum.txt file and calculate
-    //min, max and avg value. Then print the values.
-
-
-}
--- coding_prep/meta/Battleship.java
+++ coding_prep/meta/Battleship.java
@@ -1,9 +1,20 @@
 package meta;
 
+/*
+ - create a matrix(r,c,g)
+ - populate the matrix with (0,1)
+ - print the prob= (num.of.1) / total
+ [end]
+ */
+
 public class Battleship {
 
     public double Probability(int R, int C, int[][] G){
         //int[][] G = new int[R][C];
+        int count = 0;
+
+
+        return count;
 
 
     }
