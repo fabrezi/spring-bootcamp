c763cd1ae0a7ed5046e6c6c17a25ba9e57e37a87
fabrezi <fkschon@gmail.com>
2025-03-28 14:22:48 -0400

    add: link

--- coding_prep/neetcode/contains_Duplicate.java
+++ coding_prep/neetcode/contains_Duplicate.java
@@ -3,12 +3,7 @@ package neetcode;
 public class contains_Duplicate {
 
     public static void main(String[] args){
-       int x1 = 2;
-       int x2 = 3;
-
-       int x3 = x1 + x2;
-
-       System.out.println(x3);
+        System.out.println("Hola");
     }
 
 
