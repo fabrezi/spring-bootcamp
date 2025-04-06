67578384afc7f950126f752d3abbc1782b9b4025
fabrezi <fkschon@gmail.com>
2025-04-06 05:17:48 -0400

    add: exercise1

--- coding_prep/neetcode/contains_Duplicate.java
+++ coding_prep/neetcode/contains_Duplicate.java
@@ -2,11 +2,28 @@ package neetcode;
 
 public class contains_Duplicate {
 
-    public boolean duplicate(int[] nums){
+    public static boolean duplicate(int[] nums){
+        if(nums.length == 0){
+            return false;
+        }
+        for(int i=0; i < nums.length; i++){
+            for(int j=i+1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public static void main(String[] args){
+        int[] arr1 = new int[]{1, 2, 3};
+        int[] arr2 = new int[]{};
+        int[] arr3 = new int[]{1,2,3,1,2,3};
+       // int arr2 = [1,2,3,3];
+        System.out.println(duplicate(arr1));
+        System.out.println(duplicate(arr2));
+        System.out.println(duplicate(arr3));
 
     }
 
